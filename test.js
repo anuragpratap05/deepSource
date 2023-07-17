6a2505d6dbb2793da662e8730a9bbf45aa7259e9
Anurag singh <anurag.singh@american-technology.net>
2023-07-17 21:55:53 +0530

    small change

--- test.js
+++ test.js
@@ -20,7 +20,7 @@ const startExec = async () => {
 	let attendance2 = 0;
 	let attendance3 = 0;
 
-	if (student1.attendance) attendance1 = student1.attendance;
+	if (student1 && student1.attendance) attendance1 = student1.attendance;
 	if (student2.attendance) attendance2 = student2.attendance;
 	if (student3.attendance) attendance3 = student3.attendance;
 
